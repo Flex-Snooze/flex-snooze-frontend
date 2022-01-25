b1721e157d9b12120c392aa54f27b62aaf60d62d
panamabilly <billsweeney85@gmail.com>
2022-01-25 13:54:36 -0600

    change in dev-bill branch

--- src/App.js
+++ src/App.js
@@ -2,16 +2,7 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-	return (
-  <div>test from George
-
-    This is from Rachel
-
-    This is Ashleigh
-    
-		test from George This is from Rachel new test from george555555
-  </div>
-  );
+	return <div>test from George This is from Bill</div>;
 }
 
 export default App;

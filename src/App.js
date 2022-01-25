2c3d43a1af4a02de5414c6e03b02d3701dd18d2c
georgemaganzini <hyped@teamliquid.com>
2022-01-25 11:37:50 -0800

    New change to dev

--- src/App.js
+++ src/App.js
@@ -2,10 +2,9 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-	return <div>test from George
-
-    This is from Rachel
-  </div>;
+	return (
+		<div>test from George This is from Rachel new test from george555555</div>
+	);
 }
 
 export default App;

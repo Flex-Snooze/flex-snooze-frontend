fb9586450658e8b51d8151ee84f0e8ffbefd3bef
rmagley <rachelmagley@gmail.com>
2022-01-25 17:24:46 -0500

    scaffold out all components, routes, links

---
+++ src/components/Dashboard/Dashboard.js
@@ -0,0 +1,15 @@
+import React from 'react'; 
+import Log from '../Log/Log';
+import UserData from '../UserData/UserData';
+
+function Dashboard(props) {
+    return (
+        <div>
+            Hello from Dashboard
+            <UserData />
+            <Log />
+        </div>
+    );
+}
+
+export default Dashboard;
---
+++ src/components/UserData/UserData.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function UserData(props) {
+    return (
+        <div>
+            hello from user data
+        </div>
+    );
+}
+
+export default UserData;

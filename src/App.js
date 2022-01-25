47d700616d6b21bf7cc70d21ee43fca30a017e18
rmagley <rachelmagley@gmail.com>
2022-01-25 14:23:40 -0500

    rachel change

--- src/App.js
+++ src/App.js
@@ -2,7 +2,10 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-	return <div>test from George</div>;
+	return <div>test from George
+
+    This is from Rachel
+  </div>;
 }
 
 export default App;

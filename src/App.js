6d3bbe24aad2c42fd3ffe06f8e849d1cd14a305b
rmagley <rachelmagley@gmail.com>
2022-01-26 11:38:51 -0500

    State context added.
    co-authored by: Ashleigh Waller arwaller7@gmail.com
    co-authored by: Bill Sweeney billsweeney85.gmail.com

--- src/App.js
+++ src/App.js
@@ -1,20 +1,30 @@
-import logo from './logo.svg';
+
+import { Route, Routes, Link } from 'react-router-dom';
+import { useState } from 'react';
+import { WorkoutContext } from '../src/workoutContext';
+
 import './App.css';
 import Nav from './components/Nav/Nav';
 import Dashboard from './components/Dashboard/Dashboard';
-import { Route, Routes, Link } from 'react-router-dom';
 import TodayWorkout from './components/TodayWorkout/TodayWorkout';
 import About from './components/About/About'
 
 function App() {
+	const [todayWorkout, setTodayWorkout] = useState("Lifts");
+	const [selectWorkout, setSelectWorkout] = useState("Pull");
+	const [customWorkout, setCustomWorkout] = useState("Cardio");
+	const [modifyCustom, setModifyCustom] = useState(["pull up", "bentover row"]);
+
 	return (
 		<div>
-			<Nav />
-			<Routes>
-				<Route path='/' element={<TodayWorkout />} />
-				<Route path='/mydashboard' element={<Dashboard />} />
-				<Route path='/about' element={<About />} />
-			</Routes>
+			<WorkoutContext.Provider value={{ todayWorkout, setTodayWorkout}, {selectWorkout, setSelectWorkout}, {customWorkout, setCustomWorkout}, {modifyCustom, setModifyCustom}}>
+				<Nav />
+				<Routes>
+					<Route path='/' element={<TodayWorkout />} />
+					<Route path='/mydashboard' element={<Dashboard />} />
+					<Route path='/about' element={<About />} />
+				</Routes>
+			</WorkoutContext.Provider>
 		</div>
 	);
 }

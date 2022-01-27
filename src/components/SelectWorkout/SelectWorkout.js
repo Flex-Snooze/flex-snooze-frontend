236b36884c782b24800cd6eb01a183126d4cd27d
rmagley <rachelmagley@gmail.com>
2022-01-26 19:48:56 -0500

    api data pulled in for selectworkout component

--- src/components/SelectWorkout/SelectWorkout.js
+++ src/components/SelectWorkout/SelectWorkout.js
@@ -9,30 +9,34 @@ function SelectWorkout(props) {
 	const { todayWorkout, setTodayWorkout } = useContext(WorkoutContext);
 	const { finalWorkout, setFinalWorkout} = useContext(WorkoutContext);;
 
-	// useEffect(() => {
+
+	useEffect(() => {
 		fetch('http://localhost:4000/api/workout')
 			.then((res) => res.json())
 			.then((json) => {
-				// setFinalWorkout(json);
-				console.log(json);
+				setFinalWorkout(json);
+				console.log(finalWorkout[1]);
+
+				console.log(`this is state ${todayWorkout}`)
 			})
 			.catch(console.error);
-	// }, []);
+	}, []);
+
 
 
 	return (
 		<div className='selectWorkoutContainer'>
 			<div className='workoutTypeContainer'>
 				<h2> Today's {todayWorkout} Workout: </h2>
-				<div>Workout 1</div>
+				<div>{finalWorkout[1].exercises[0]}</div>
 				<button className='next-btn'>Next Excercise</button>
-				<div>Workout 2</div>{' '}
+				<div>{finalWorkout[1].exercises[1]}</div>{' '}
 				<button className='next-btn'>Next Excercise</button>
-				<div>Workout 3</div>
+				<div>{finalWorkout[1].exercises[2]}</div>
 				<button className='next-btn'>Next Excercise</button>
-				<div>Workout 4</div>
+				<div>{finalWorkout[1].exercises[3]}</div>
 				<button className='next-btn'>Next Excercise</button>
-				<div>Workout 5</div>
+				<div>{finalWorkout[1].exercises[4]}</div>
 				<button className='next-btn'>Next Excercise</button>
 				<button className='ok-btn'>Let's Go</button>
 			</div>

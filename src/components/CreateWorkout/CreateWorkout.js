30282f38294258f4c1fdf37102faa923107e1170
panamabilly <billsweeney85@gmail.com>
2022-01-27 17:02:55 -0600

    CreateWorkout.js connected to back end and posting form data

--- src/components/CreateWorkout/CreateWorkout.js
+++ src/components/CreateWorkout/CreateWorkout.js
@@ -59,18 +59,7 @@ function CreateWorkout(props) {
 					onChange={handleChange}
 					value={formState.date}
 				/>
-				{/* <label htmlFor='starttimefield'>Start Time:</label> */}
-				{/* <input type='time' id='timeFieldStart' />
-				<label htmlFor='endtimefield'> End Time:</label>
-				<input type='time' id='timeFieldEnd' /> */}
-				{/* <label htmlFor='excercisetype'>Excercise Type:</label> */}
-				{/* <select id='excerciseType'>
-					<option value='push'>Push</option>
-					<option value='pull'>Pull</option>
-					<option value='legs'>Legs</option>
-					<option value='yoga'>Yoga</option>
-					<option value='custom'>Other</option>
-				</select> */}
+
 				<label htmlFor='exercises'>Workout Notes:</label>
 				<textarea
 					id='exercises'

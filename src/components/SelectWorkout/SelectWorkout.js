3b61f2360617e060b6b7ec857b3aeb281163b4eb
rmagley <rachelmagley@gmail.com>
2022-01-26 19:21:09 -0500

    api connected

--- src/components/SelectWorkout/SelectWorkout.js
+++ src/components/SelectWorkout/SelectWorkout.js
@@ -1,6 +1,6 @@
 import './SelectWorkout.css';
 import React from 'react';
-import {useContext} from 'react'
+import {useContext, useEffect} from 'react'
 import { WorkoutContext } from '../../workoutContext';
 
 
@@ -8,6 +8,17 @@ function SelectWorkout(props) {
 
 	const { todayWorkout, setTodayWorkout } = useContext(WorkoutContext);
 	const { finalWorkout, setFinalWorkout} = useContext(WorkoutContext);;
+
+	// useEffect(() => {
+		fetch('http://localhost:4000/api/workout')
+			.then((res) => res.json())
+			.then((json) => {
+				// setFinalWorkout(json);
+				console.log(json);
+			})
+			.catch(console.error);
+	// }, []);
+
 
 	return (
 		<div className='selectWorkoutContainer'>

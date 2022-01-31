871da32c3d1aa9ccc3393c69437d258b3f38bee7
georgemaganzini <hyped@teamliquid.com>
2022-01-31 05:24:53 -0800

    Re-wire api calls to new paths

--- src/components/Log/LogDetail.js
+++ src/components/Log/LogDetail.js
@@ -2,10 +2,28 @@ import React from 'react';
 import { useContext, useState, useEffect } from 'react';
 import { WorkoutContext } from '../../workoutContext';
 import './LogDetail.css';
+import axios from 'axios';
 
 function LogDetail(props) {
 	const { logId, userWorkoutData } = useContext(WorkoutContext);
 	const [loading, setLoading] = useState(true);
+
+	function handleClick() {
+		console.log(userWorkoutData[logId]._id);
+		console.log(logId);
+
+		axios
+			.delete(
+				// 'https://flex-five.herokuapp.com/api/user/5',
+				`https://flex-five.herokuapp.com/api/log/${userWorkoutData[logId]._id}`
+			)
+			// .then((response) => {
+			// 	setToDashboard(true);
+			// })
+			.catch((error) => {
+				console.error('Something went wrong!', error);
+			});
+	}
 
 	useEffect(() => {
 		const handleLoadingTimeOut = setTimeout(() => {
@@ -35,6 +53,9 @@ function LogDetail(props) {
 						  ))
 						: userWorkoutData[logId].exercises}
 				</div>
+				<button className='logHeader__button' onClick={handleClick}>
+					Remove Entry
+				</button>
 			</section>
 		);
 	} else return <h2>Select a workout to see details!</h2>;
